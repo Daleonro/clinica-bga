8d0574d3ee5146b4c7bd88b566c08ff765ee829f
Daleonro <daleonro@outlook.com>
2022-06-11 10:14:44 -0500

    Se agregan la validaciones desde el backend al formulario de ingreso paciente

--- src/routes/paciente.js
+++ src/routes/paciente.js
@@ -1,13 +1,26 @@
 const router = require('express').Router();
 
+const { body, validationResult } = require('express-validator');
+
+
 require('../models/Pacientes');
 
 router.get('/ingreso-paciente', (req,res) => {
     res.render('ingreso-paciente');
 });
 
-router.post('/nuevo-paciente', (req,res) => {
+router.post('/nuevo-paciente', [
+    body('cedula', 'Ingrese un numero de cedula valido sin comas ni puntos').exists().isNumeric().isLength({min:6}),
+    body('nombrePaciente', 'Ingrese un nombre valido').exists().isString(),
+    body('apellidoPaciente', 'Ingrese un apellido valido').exists().isString(),
+    body('numeroPaciente', 'Ingrese un numero de telefono valido').exists().isNumeric().isLength({min:7}),
+    body('direccionPaciente', 'Ingrese una direccion valida').exists(),
+    body('ciudadPaciente', 'Ingrese una ciudad valida').exists(),
+    body('historiaClinica', 'Ingrese la historia clinica del paciente').exists(),
+    body('date', 'Ingrese la fecha de la consulta').exists()
+], (req,res) => {
     console.log(req.body);
+    const { tipoDocumento, cedula, nombrePaciente, apellidoPaciente, numeroPaciente, direccionPaciente, ciudadPaciente, historiaClinica, date } = req.body;
     res.send('Datos enviados :D')
 });
 
